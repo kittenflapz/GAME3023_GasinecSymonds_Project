1df70f3748f91a2d832eda5e3c52e3520415ce3a
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-10-04 14:46:09 -0400

    initial commit

---
+++ Assets/Characters/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (speed == 0)
+        { 
+            speed = 5; 
+        }
+
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Hello World"); 
+        Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementVector *= speed;
+        rigidBody.velocity = movementVector;
+
+        // transform.Translate(movementVector, Space.World);
+    }
+}
---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(playerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
