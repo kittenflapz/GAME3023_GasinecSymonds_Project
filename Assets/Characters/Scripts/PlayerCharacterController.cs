7997d2898a593977c319629cf6feaeac9ad3b067
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-10-11 12:53:24 -0400

    Player walk + idle animations in 4 directions (no diagonals)

--- Assets/Characters/Scripts/PlayerCharacterController.cs
+++ Assets/Characters/Scripts/PlayerCharacterController.cs
@@ -10,6 +10,11 @@ public class PlayerCharacterController : MonoBehaviour
     [SerializeField]
     Rigidbody2D rigidBody;
 
+
+    // Animation variables
+    Animator animator;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,18 +22,31 @@ public class PlayerCharacterController : MonoBehaviour
         { 
             speed = 5; 
         }
-
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("Hello World"); 
+        // Movement
+
         Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         movementVector *= speed;
         rigidBody.velocity = movementVector;
 
-        // transform.Translate(movementVector, Space.World);
+        // Animation
+
+        animator.SetFloat("moveX", rigidBody.velocity.x);
+        animator.SetFloat("moveY", rigidBody.velocity.y);
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1  || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            animator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
     }
+
+
+
 }
