11827a0d4c66f5bf9efa718e0cf8e52346a3ba8c
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-10-11 18:53:34 -0400

    reorganized folder structure and implemented random idle fidget anims

--- Assets/_Scripts/PlayerCharacterController.cs
+++ Assets/_Scripts/PlayerCharacterController.cs
@@ -10,9 +10,9 @@ public class PlayerCharacterController : MonoBehaviour
     [SerializeField]
     Rigidbody2D rigidBody;
 
-
     // Animation variables
     Animator animator;
+    float fidgetTimer;
 
 
     // Start is called before the first frame update
@@ -45,8 +45,29 @@ public class PlayerCharacterController : MonoBehaviour
             animator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
             animator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
         }
+
+        // If (random 3-5 seconds) pass while idle, set the fidget chance to 1 and then set it back after a second
+        if(Input.anyKeyDown) // input received, reset the fidget timer
+        {
+            fidgetTimer = 0;
+        }
+        else
+        {
+            fidgetTimer++;
+
+            if (fidgetTimer >= Random.Range(400, 700))
+            {
+                StartCoroutine(setFidgetChance());
+            }
+        }
+  
     }
 
-
-
+    IEnumerator setFidgetChance()
+    {
+        animator.SetFloat("fidgetChance", 1); // make it 100% likely that the player will fidget
+        yield return new WaitForSeconds(1f);
+        animator.SetFloat("fidgetChance", 0); // make it 0% likely that the player will fidget
+        fidgetTimer = 0; // set the timer back to 0 so he's not just constantly fidgeting forever
+    }
 }
