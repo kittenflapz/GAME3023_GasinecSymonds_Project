f7bf6b99c33c88a3652cb5e11cf2baf823b7d9a0
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-11-15 15:22:57 -0500

    battle system setup

---
+++ Assets/_Scripts/Battle/BattleSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+enum BattlePhase
+{
+    PLAYER,
+    ENEMY,
+    COUNT
+}
+
+
+public class BattleSystem : MonoBehaviour
+{
+
+    [SerializeField]
+    BattlePhase phase;
+    
+    [SerializeField]
+    ICharacter[] combatants;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AdvanceTurns();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AdvanceTurns()
+    {
+        phase++;
+        if(phase > BattlePhase.COUNT)
+        {
+            phase = 0;
+        }
+        Debug.Log("It is " + combatants[(int)phase].name + "'s turn");
+        combatants[(int)phase].TakeTurn();
+    }
+}
---
+++ Assets/_Scripts/Battle/ICharacter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ICharacter : MonoBehaviour
+{
+    public int hp;
+
+    [SerializeField]
+    private int hpMax = 10;
+
+    [SerializeField]
+    Ability[] abilities = new Ability[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UseAbility(int id)
+    {
+       // abilities[id].whatever
+    }
+
+    public void TakeTurn()
+    {
+
+    }
+}
